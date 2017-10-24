250a72176563335e0eabe31a3408744e70d26d52
frcocoatst <friedrichhaeupl@friedrichs-mbp.fritz.box>
2017-10-24 10:24:28 +0200

    Initial Commit

---
+++ FormatterDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FormatterDemo
+//
+//  Created by Friedrich HAEUPL on 24.10.17.
+//  Copyright © 2017 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
